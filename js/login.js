7040cb436fb7be1054859e6ef87335934280fa30
ropi-rmt <ropi.rmt@gmail.com>
2026-03-11 14:48:55 -0300

    adding dark mode en login

--- js/login.js
+++ js/login.js
@@ -33,3 +33,16 @@ form.addEventListener("submit", function(e) {
     }
 
 });
+
+const colorModeButton = document.querySelector("#color-mode");
+const body = document.body;
+
+colorModeButton.addEventListener("click", cambiarModoColor);
+function cambiarModoColor() {
+    body.classList.toggle("dark-mode");
+    if (body.classList.contains("dark-mode")) {
+        colorModeButton.innerText = "Cambiar a Light Mode";
+    } else {
+        colorModeButton.innerText = "Cambiar a Dark Mode";
+    }
+}
